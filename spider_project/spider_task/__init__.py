ab45788162082d72f1c0b19b51f81a8539354d0e
JasonLearning <616273218@qq.com>
2017-03-05 14:30:19 +0800

    2017-03-05

---
+++ spider_project/spider_task/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/3/5 下午1:33
+# @Author  : Jason
+# @Site    : 
+# @File    : __init__.py.py
---
+++ spider_project/spider_task/spider.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/3/5 下午1:34
+# @Author  : Jason
+# @Site    : 
+# @File    : spider.py
